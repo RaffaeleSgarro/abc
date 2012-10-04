0544dff07df99868aab638013b488c62b510b024
Raffaele <raffaele@gmail.com>
2012-10-04 21:16:50 +0200

    Android project

---
+++ src/com/zybnet/abc/AbbecedarioActivity.java
@@ -0,0 +1,15 @@
+package com.zybnet.abc;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AbbecedarioActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
