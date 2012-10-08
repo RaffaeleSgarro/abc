fe47de531631ba4c794e6bb8165ec92504c7562f
Raffaele <raffaele@gmail.com>
2012-10-08 11:29:13 +0200

    Removed unused import

--- src/com/zybnet/abc/AbbecedarioActivity.java
+++ src/com/zybnet/abc/AbbecedarioActivity.java
@@ -7,7 +7,6 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
-import com.zybnet.abc.fragments.Cover;
 import com.zybnet.abc.fragments.Exams;
 import com.zybnet.abc.fragments.TimeTable;
 
