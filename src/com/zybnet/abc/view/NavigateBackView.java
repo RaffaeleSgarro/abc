1f3b1c50fecd84c092440b21173acfac7959b4e4
Raffaele <raffaele@gmail.com>
2012-10-14 20:32:57 +0200

    Some actionbar improvements

--- src/com/zybnet/abc/view/NavigateBackView.java
+++ src/com/zybnet/abc/view/NavigateBackView.java
@@ -2,10 +2,13 @@ package com.zybnet.abc.view;
 
 import java.util.Stack;
 
+import com.zybnet.abc.R;
+
 import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.ImageButton;
 import android.widget.ViewAnimator;
 
@@ -27,20 +30,31 @@ public class NavigateBackView extends ImageButton {
 
 	public void addItem(ViewAnimator animator, Animation in, Animation out) {
 		history.push(new Item(animator, in, out));
+		if (history.size() == 1) {
+			startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.alpha_in));
+		}
 	}
 	
 	private class Back implements View.OnClickListener {
 		@Override
 		public void onClick(View view) {
-			if (history.size() == 0)
-				return;
-			
-			// TODO remove listeners
-			Item item = history.pop();
-			item.animator.setInAnimation(item.in);
-			item.animator.setOutAnimation(item.out);
-			item.animator.showPrevious();
-			// TODO setup listeners
+			back();
+		}
+	}
+	
+	public void back() {
+		if (history.size() == 0)
+			return;
+		
+		// TODO remove listeners
+		Item item = history.pop();
+		item.animator.setInAnimation(item.in);
+		item.animator.setOutAnimation(item.out);
+		item.animator.showPrevious();
+		// TODO setup listeners
+		
+		if (history.size() == 0) {
+			startAnimation(AnimationUtils.loadAnimation(getContext(), R.anim.alpha_out));
 		}
 	}
 	
